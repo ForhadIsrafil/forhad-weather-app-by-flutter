e155841471f85cf8f6dc74a4806a3fb6a5857c1a
forhadisrafil <forhadtuhin5088@gmail.com>
2022-10-09 05:03:18 +0600

    detaisl wip

--- lib/weatherDetails.dart
+++ lib/weatherDetails.dart
@@ -63,31 +63,116 @@ class WeatherDetails extends StatelessWidget {
             ),
             Container(
               color: Colors.white,
-              height: 200.0,
+              height: 205.0,
+              child: ListView(
+                padding: const EdgeInsets.all(1.0),
+                children: const [
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.lightGreen,
+                      child: Icon(Icons.cloud, color: Colors.white),
+                    ),
+                    title: Text(
+                      "Yesterday",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      "Cloudy",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Text(
+                      "30/27 °C",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    textColor: Colors.blueGrey,
+                  ),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.lightGreen,
+                      child: Icon(Icons.cloud, color: Colors.white),
+                    ),
+                    title: Text(
+                      "Today",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      "Husky",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Text(
+                      "30/27 °C",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    textColor: Colors.blueGrey,
+                  ),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.lightGreen,
+                      child: Icon(Icons.cloud, color: Colors.white),
+                    ),
+                    title: Text(
+                      "Tomorrow",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      "Cloudy",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Text(
+                      "30/27 °C",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    textColor: Colors.blueGrey,
+                  ),
+                ],
+              ),
             ),
             Container(
               color: Colors.indigo,
-              height: 200.0,
+              padding: const EdgeInsets.all(5.0),
+              height: 50.0,
+              child: const Text(
+                "Next 7 day Weather of the country",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
             ),
+            Container(
+              color: Colors.indigo,
+              padding: const EdgeInsets.all(5.0),
+              height: 400.0,
+              child: Row(
+                children: [
+                  Column(
+
+                    children: [
+                      Row(
+                        children: const [
+                          Text("data"),
+                          Text("data"),
+                          Text("data"),
+                          Text("data"),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-//
 // Text(
-// "Country Name",
-// style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-// textAlign: TextAlign.center,
-// ),
-// Text(
-// "27°C",
-// style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-// textAlign: TextAlign.center,
-// ),
-// Text(
-// "Cloudy",
-// style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
-// textAlign: TextAlign.center,
+// "Next 7 day Weather of the country",
+// // textAlign: TextAlign.center,
+// style: TextStyle(
+// fontSize: 20.0,
+// fontWeight: FontWeight.bold,
+// color: Colors.white),
 // ),
