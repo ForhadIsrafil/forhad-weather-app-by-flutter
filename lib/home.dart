14a606a31cf6cc49183cc35872587d56a514d3d5
forhadisrafil <forhadtuhin5088@gmail.com>
2022-11-13 05:15:33 +0600

    api wip

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:if_weather/weatherDetails.dart';
 import 'utils/bar.dart';
-import 'package:requests/requests.dart';
+import 'utils/default_weather.dart';
+import 'dart:convert';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -33,6 +34,7 @@ class _HandleWeatherInputState extends State<HandleWeatherInput> {
   bool isError = false;
 
   final _inputCountryController = TextEditingController();
+  late Future<Weather> response;
 
   @override
   void initState() {
@@ -40,10 +42,7 @@ class _HandleWeatherInputState extends State<HandleWeatherInput> {
     super.initState();
 
     _inputCountryController.addListener(_updateCountry);
-
-    //  get default weather data start
-
-    //  get default weather data end
+    response = getDefaultData();
   }
 
   void _updateCountry() {
@@ -51,12 +50,6 @@ class _HandleWeatherInputState extends State<HandleWeatherInput> {
       _inputCountry = _inputCountryController.text;
     });
   }
-
-  // void countryValue(value) {
-  //   setState(() {
-  //     defaultCountry = value;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -153,182 +146,198 @@ class _HandleWeatherInputState extends State<HandleWeatherInput> {
                 child: const Text("Submit"),
               ),
             ),
+            // Container(
+            //   alignment: Alignment.center,
+            //   width: 50.0,
+            //   // color: Colors.black87,
+            //   child: DropdownButton(
+            //       value: _defaultCountry,
+            //       dropdownColor: Colors.black45,
+            //       focusColor: Colors.indigoAccent,
+            //       borderRadius: BorderRadius.circular(7.0),
+            //       icon: const Icon(Icons.pin_drop, color: Colors.cyan),
+            //       style: const TextStyle(color: Colors.tealAccent),
+            //       alignment: Alignment.center,
+            //       items: [
+            //         DropdownMenuItem(
+            //           value: "City",
+            //           enabled: false,
+            //           alignment: Alignment.centerRight,
+            //           child: const Text("City"),
+            //           onTap: () {
+            //             setState(() {
+            //               _defaultCountry = "City";
+            //             });
+            //           },
+            //         ),
+            //         DropdownMenuItem(
+            //           value: "Dhaka",
+            //           alignment: Alignment.centerRight,
+            //           child: const Text("Dhaka"),
+            //           onTap: () {
+            //             setState(() {
+            //               _defaultCountry = "Dhaka";
+            //             });
+            //           },
+            //         ),
+            //         DropdownMenuItem(
+            //           value: "Kabul",
+            //           alignment: Alignment.centerRight,
+            //           child: const Text("Kabul"),
+            //           onTap: () {
+            //             setState(() {
+            //               _defaultCountry = "Kabul";
+            //             });
+            //           },
+            //         ),
+            //         DropdownMenuItem(
+            //           value: "Delhi",
+            //           alignment: Alignment.centerRight,
+            //           child: const Text("Delhi"),
+            //           onTap: () {
+            //             setState(() {
+            //               _defaultCountry = "Delhi";
+            //             });
+            //           },
+            //         ),
+            //       ],
+            //       onChanged: (String? value) {}),
+            // ),
+            // Container(
+            //   // color: Colors.cyan,
+            //   height: 200.0,
+            //   width: 150.0,
+            //   decoration: BoxDecoration(
+            //       color: Colors.teal,
+            //       border: Border.all(
+            //         color: Colors.blueGrey,
+            //         width: 6.0,
+            //       ),
+            //       shape: BoxShape.rectangle,
+            //       borderRadius: BorderRadius.circular(15.0)),
+            //   alignment: Alignment.center,
+            //   margin: const EdgeInsets.only(top: 100.0),
+            //   padding: const EdgeInsets.all(10.0),
+            //   child: ListView(
+            //     padding: const EdgeInsets.all(10.0),
+            //     children: [
+            //       Text.rich(TextSpan(style: TextStyle(), children: [
+            //         const TextSpan(
+            //             text: "Dhaka\n",
+            //             style: TextStyle(
+            //               fontSize: 35.0,
+            //               fontWeight: FontWeight.bold,
+            //               color: Colors.white,
+            //             )),
+            //         TextSpan(
+            //           text: "Temperature: $response°\n",
+            //           style: const TextStyle(
+            //               fontSize: 20.0,
+            //               fontWeight: FontWeight.bold,
+            //               color: Colors.white),
+            //         ),
+            //         const TextSpan(
+            //           text: "Clear sky\n",
+            //           style: TextStyle(
+            //             fontSize: 15.0,
+            //             fontWeight: FontWeight.bold,
+            //             color: Colors.white,
+            //           ),
+            //         ),
+            //         const WidgetSpan(
+            //             child: Image(
+            //           image: NetworkImage(
+            //               "https://openweathermap.org/img/wn/01n@2x.png"),
+            //           color: Colors.yellow,
+            //           alignment: Alignment.center,
+            //           fit: BoxFit.fill,
+            //         )),
+            //       ])),
+            //     ],
+            //   ),
+            // ),
+            // Stack(
+            //   alignment: AlignmentDirectional.center,
+            //   // fit: StackFit.expand,
+            //   children: [
+            //     SizedBox(
+            //       // height: 50,
+            //       width: MediaQuery.of(context).size.width,
+            //       // height: random.nextInt(200).toDouble() >= 100 ? random.nextInt(200).toDouble() : 200.0,
+            //       // color: Color.fromARGB(random.nextInt(255), random.nextInt(255),
+            //       //     random.nextInt(255), random.nextInt(255)),
+            //
+            //       child: CustomPaint(
+            //         painter: ShapePainter(),
+            //       ),
+            //     ),
+            //     Container(
+            //       alignment: Alignment.topLeft,
+            //       padding:
+            //           const EdgeInsets.only(bottom: 100, left: 10, top: 10),
+            //       child: Column(
+            //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //         children: const [
+            //           Icon(
+            //             Icons.sunny,
+            //             size: 50,
+            //             color: Colors.yellow,
+            //           ),
+            //           Text(
+            //             "Sunrise",
+            //             textAlign: TextAlign.center,
+            //             style: TextStyle(
+            //                 fontWeight: FontWeight.bold, color: Colors.red),
+            //           ),
+            //           Text(
+            //             "5:50",
+            //             style: TextStyle(fontWeight: FontWeight.bold),
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //     Container(
+            //       alignment: Alignment.topRight,
+            //       padding:
+            //           const EdgeInsets.only(bottom: 100, right: 10, top: 10),
+            //       child: Column(
+            //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //         children: const [
+            //           Icon(
+            //             Icons.sunny,
+            //             size: 50,
+            //             color: Colors.black54,
+            //           ),
+            //           Text(
+            //             "Sunset",
+            //             textAlign: TextAlign.center,
+            //             style: TextStyle(
+            //                 fontWeight: FontWeight.bold,
+            //                 color: Colors.blueGrey),
+            //           ),
+            //           Text(
+            //             "5:50",
+            //             style: TextStyle(fontWeight: FontWeight.bold),
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //   ],
+            // ),
             Container(
-              alignment: Alignment.center,
-              width: 50.0,
-              // color: Colors.black87,
-              child: DropdownButton(
-                  value: _defaultCountry,
-                  dropdownColor: Colors.black45,
-                  focusColor: Colors.indigoAccent,
-                  borderRadius: BorderRadius.circular(7.0),
-                  icon: const Icon(Icons.pin_drop, color: Colors.cyan),
-                  style: const TextStyle(color: Colors.tealAccent),
-                  alignment: Alignment.center,
-                  items: [
-                    DropdownMenuItem(
-                      value: "City",
-                      enabled: false,
-                      alignment: Alignment.centerRight,
-                      child: const Text("City"),
-                      onTap: () {
-                        setState(() {
-                          _defaultCountry = "City";
-                        });
-                      },
-                    ),
-                    DropdownMenuItem(
-                      value: "Dhaka",
-                      alignment: Alignment.centerRight,
-                      child: const Text("Dhaka"),
-                      onTap: () {
-                        setState(() {
-                          _defaultCountry = "Dhaka";
-                        });
-                      },
-                    ),
-                    DropdownMenuItem(
-                      value: "Kabul",
-                      alignment: Alignment.centerRight,
-                      child: const Text("Kabul"),
-                      onTap: () {
-                        setState(() {
-                          _defaultCountry = "Kabul";
-                        });
-                      },
-                    ),
-                    DropdownMenuItem(
-                      value: "Delhi",
-                      alignment: Alignment.centerRight,
-                      child: const Text("Delhi"),
-                      onTap: () {
-                        setState(() {
-                          _defaultCountry = "Delhi";
-                        });
-                      },
-                    ),
-                  ],
-                  onChanged: (String? value) {}),
-            ),
-            Container(
-              // color: Colors.cyan,
-              height: 200.0,
-              width: 150.0,
-              decoration: BoxDecoration(
-                  color: Colors.teal,
-                  border: Border.all(
-                    color: Colors.blueGrey,
-                    width: 6.0,
-                  ),
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(15.0)),
-              alignment: Alignment.center,
-              margin: const EdgeInsets.only(top: 100.0),
-              padding: const EdgeInsets.all(10.0),
-              child: ListView(
-                padding: const EdgeInsets.all(10.0),
-                children: const [
-                  Text.rich(TextSpan(style: TextStyle(), children: [
-                    TextSpan(
-                        text: "Dhaka\n",
-                        style: TextStyle(
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        )),
-                    TextSpan(
-                      text: "Temperature: 30°\n",
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    TextSpan(
-                      text: "Clear sky\n",
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    WidgetSpan(
-                        child: Image(
-                      image: NetworkImage(
-                          "https://openweathermap.org/img/wn/01n@2x.png"),
-                      color: Colors.yellow,
-                      alignment: Alignment.center,
-                      fit: BoxFit.fill,
-                    )),
-                  ])),
-                ],
-              ),
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              // fit: StackFit.expand,
-              children: [
-                SizedBox(
-                  // height: 50,
-                  width: MediaQuery.of(context).size.width,
-                  // height: random.nextInt(200).toDouble() >= 100 ? random.nextInt(200).toDouble() : 200.0,
-                  // color: Color.fromARGB(random.nextInt(255), random.nextInt(255),
-                  //     random.nextInt(255), random.nextInt(255)),
+              child: FutureBuilder<Weather>(
+                future: response,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data!.main['temp'].toString());
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
 
-                  child: CustomPaint(
-                    painter: ShapePainter(),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  padding: const EdgeInsets.only(bottom: 100, left: 10, top: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      Icon(
-                        Icons.sunny,
-                        size: 50,
-                        color: Colors.yellow,
-                      ),
-                      Text(
-                        "Sunrise",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.red),
-                      ),
-                      Text(
-                        "5:50",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.topRight,
-                  padding:
-                      const EdgeInsets.only(bottom: 100, right: 10, top: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      Icon(
-                        Icons.sunny,
-                        size: 50,
-                        color: Colors.black54,
-                      ),
-                      Text(
-                        "Sunset",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blueGrey),
-                      ),
-                      Text(
-                        "5:50",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator();
+                },
+              ),
             ),
           ],
         ),
@@ -337,35 +346,11 @@ class _HandleWeatherInputState extends State<HandleWeatherInput> {
   }
 }
 
-// const Text.rich(TextSpan(style: TextStyle(), children: [
-// TextSpan(
-// text: "Dhaka\n",
-// style: TextStyle(
-// fontSize: 35.0,
-// fontWeight: FontWeight.bold,
-// color: Colors.white,
-// )),
-// TextSpan(
-// text: "Temperature: 30°\n",
-// style: TextStyle(
-// fontSize: 20.0,
-// fontWeight: FontWeight.bold,
-// color: Colors.white),
-// ),
-// TextSpan(
-// text: "Clear sky\n",
-// style: TextStyle(
-// fontSize: 15.0,
-// fontWeight: FontWeight.bold,
-// color: Colors.black54,
-// ),
-// ),
-// WidgetSpan(
-// child: Image(
-// image: NetworkImage(
-// "https://openweathermap.org/img/wn/01n@2x.png"),
-// color: Colors.yellow,
-// alignment: Alignment.center,
-// fit: BoxFit.fill,
-// )),
-// ])),
+//  get default weather data start
+// data() {
+//   var res = getDefaultData();
+//   if (res[0] != false) {
+//     return res[1];
+//   }
+// }
+// //  get default weather data end
--- lib/utils/default_weather.dart
+++ lib/utils/default_weather.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-
 import 'package:requests/requests.dart';
-import 'package:http/http.dart' as http;
-
 import 'dart:async';
 
-main() {
-  // var r = Requests.get('https://google.com');
-  // print(r.statusCode);
-  var client = http.Client();
-  try {
-    var response = client.get(Uri.https('https://openweathermap.org/img/wn/01n@2x.png'));
-    var decodedResponse = response;
-    print(decodedResponse);
-  } finally {
-    client.close();
+Future<Weather> getDefaultData() async {
+  var res = await Requests.get(
+      "https://api.openweathermap.org/data/2.5/weather?q=Dhaka&units=metric&appid=564a930b3fa90e1e2ae07e673ca72227");
+  if (res.statusCode == 200) {
+    // print(res.json());
+    // print(res.json()['main']); // tempretures details
+    // print(res.json()['weather'][0]); // details, icon
+    // print(res.json()['sys']); // sunrise, pressure
+    //
+    // print(res.json()['main']['temp']);
+    // var r = Requests.get('https://google.com');
+    // r.raiseForStatus();
+    // String body = r.content();
+    // return jsonDecode(res.content());
+    return Weather.fromJson(jsonDecode(res.body));
+  } else {
+    throw Exception('Failed to load data');
   }
-  return true;
+}
+
+class Weather {
+  final main;
+  final weather;
+  final sys;
+
+  const Weather({
+    required this.main,
+    required this.weather,
+    required this.sys,
+  });
+
+  factory Weather.fromJson(Map json) {
+    return Weather(
+      main: json['main'],
+      weather: json['weather'],
+      sys: json['sys'],
+    );
+  }
 }
